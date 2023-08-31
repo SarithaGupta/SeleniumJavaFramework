99e1a658239ad7a5fe41ae85d39a435fafa01d9a
Saritha KS <sarithaguptaks@gmail.com>
2023-08-31 09:56:31 -0500

    added files

---
+++ SDETAutomationPractice/src/test/java/arrays/programs.java
@@ -0,0 +1,22 @@
+ Volume in drive C is OS
+ Volume Serial Number is AAE9-4445
+
+ Directory of C:\Users\Sumod\git\SeleniumJavaFramework\SDETAutomationPractice\src\test\java\arrays
+
+08/30/2023  04:28 PM    <DIR>          .
+08/30/2023  10:01 AM    <DIR>          ..
+08/24/2023  09:25 PM             1,365 Array.java
+08/24/2023  09:25 PM             1,365 Array1.java
+08/25/2023  12:39 PM               600 bubblesort.java
+08/25/2023  12:00 PM               439 bubbleSortbyPavan.java
+08/25/2023  12:44 PM               992 FindDuplicateElementInArray.java
+08/24/2023  09:26 PM               363 Interview_MaximumNumber.java
+08/24/2023  09:26 PM             1,120 Interview_minimumNumber.java
+08/24/2023  09:27 PM             1,078 javaArray.java
+08/25/2023  12:55 PM               698 missingNumberInArray.java
+08/24/2023  09:26 PM             1,257 MultiDimensionalArray.java
+08/30/2023  04:28 PM                 0 programs.java
+08/29/2023  02:28 PM               453 searhElementUsingLinearSearchApproach.java
+08/25/2023  12:15 PM             1,089 usingParallelSortArray.java
+              13 File(s)         10,819 bytes
+               2 Dir(s)  237,758,586,880 bytes free
